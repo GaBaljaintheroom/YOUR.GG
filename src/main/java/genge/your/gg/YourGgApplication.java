03491888f13fdc193b2a110107984bf960b83c59
GaBaljaintheroom <junsu1222@naver.com>
2024-12-05 21:23:00 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/genge/your/gg/YourGgApplication.java
@@ -0,0 +1,13 @@
+package genge.your.gg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourGgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourGgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/genge/your/gg/YourGgApplicationTests.java
@@ -0,0 +1,13 @@
+package genge.your.gg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourGgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
