788d2ab734681bb3743fd276160594a0b6450303
GaBaljaintheroom <junsu1222@naver.com>
2024-12-05 21:27:54 +0900

    chore: 구글 자바 형식 checkstyle 적용

--- src/main/java/genge/your/gg/YourGgApplication.java
+++ src/main/java/genge/your/gg/YourGgApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YourGgApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YourGgApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YourGgApplication.class, args);
+    }
 
 }
