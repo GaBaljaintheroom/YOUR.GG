585c99a382ef1943a9696e311ccaebda0e47edaa
GaBaljaintheroom <junsu1222@naver.com>
2024-12-06 10:38:54 +0900

    feat: 사용자 정보 조회 Riot API 연동 통합 테스트

--- src/test/java/geng/your/gg/RiotClientTest.java
+++ src/test/java/geng/your/gg/RiotClientTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import geng.your.gg.infrastructure.riot.RiotClient;
 import geng.your.gg.infrastructure.riot.dto.AccountDto;
+import geng.your.gg.infrastructure.riot.dto.SummonerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,6 +33,29 @@ class RiotClientTest {
             softly.assertThat(accountDto.puuid()).isNotEmpty();
             softly.assertThat(accountDto.gameName()).isNotEmpty();
             softly.assertThat(accountDto.tagLine()).isNotEmpty();
+        });
+    }
+
+    @Test
+    void getSummonerInfo() {
+        // given & when
+        String puuId = "a5KGfHPki4xBS6UXWY5rkL6Jyjk7wyiu-4bECy3J2z-4s7DVqsjlkOL2Q-gtSFaVu5fciizb36rTHA";
+        SummonerDto summonerDto;
+        try {
+            summonerDto = riotClient.getSummonerInfo(puuId);
+        } catch (Exception e) {
+            return;
+        }
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(summonerDto).isNotNull();
+            softly.assertThat(summonerDto.accountId()).isNotEmpty();
+            softly.assertThat(summonerDto.profileIconId()).isNotNull();
+            softly.assertThat(summonerDto.revisionDate()).isNotNull();
+            softly.assertThat(summonerDto.id()).isNotEmpty();
+            softly.assertThat(summonerDto.puuid()).isNotEmpty();
+            softly.assertThat(summonerDto.summonerLevel()).isNotNull();
         });
     }
 }
